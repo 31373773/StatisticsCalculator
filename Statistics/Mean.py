095babba797db6d774b8feededc895d61e98aa46
cjl72 <cjl72@njit.edu>
2020-02-11 14:21:21 -0500

    Fix: Feature test statistics

--- Statistics/Mean.py
+++ Statistics/Mean.py
@@ -1,9 +1,11 @@
 from MathOperations.addition import Addition
 from MathOperations.division import Division
+from Calculator.Calculator import Calculator
+
 
 def mean(data):
     num_values = len(data)
     total = 0
     for num in data:
-        total = addition(total, num)
-    return division(total, num_values)
+        total = Addition.sum(total, num)
+    return Division.divide(total, num_values)
--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -3,10 +3,17 @@ import unittest
 from Statistics.Mean import mean
 from Statistics.Statistics import Statistics
 
-Class MyStatisticsTestCase(unittest.TestCase)
+class MyTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.statistics = Calculator()
+    def setUp(self) -> None:
+        self.statistics = Statistics()
 
     def test_instantiate_statistics_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
+
+    def test_statistics_mean(self):
+        valueList = [3, 3, 3, 3, 3, 3]
+        self.assertEqual(3, mean(valueList))
+
+if __name__ == '__main__':
+    unittest.main()
